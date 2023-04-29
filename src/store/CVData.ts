b54629f77d1959d3006baab4f23f4113a584c644
Christian Villegas <chrisvill.2312@gmail.com>
2023-04-28 23:44:43 -0300

    add startingTypes on stream 28/4

--- src/store/CVData.ts
+++ src/store/CVData.ts
@@ -1,14 +1,55 @@
 import { create } from "zustand";
 import { persist, createJSONStorage } from "zustand/middleware";
 
-interface Session {
+export interface Social {
+  url: string;
+  title: string;
+  description?: string;
+}
+
+export interface Licenses {
+  type: string;
+  level?: string;
+  description?: string;
+}
+export interface PersonalData {
+  name: string;
+  lastName: string;
+  email: string;
+  phone?: string;
+  social?: Social[];
+  licenses?: Licenses[];
+  workAvailability?: "immediately" | "toNegotiate" | "partTime" | "temporary";
+  presentiality?: "remote" | "onSite" | "hybrid";
+  travelAvailability?: true;
+  maritalStatus?: "single" | "married" | "divorced" | "widower";
+  birthday?: Date;
+  nationality?: string;
+  residency: string;
+}
+
+export interface Experiences {
+  begin: Date;
+  end: Date | "CurrentlyWorkingHere";
+  title: string;
+  rol?: string;
+  description: string;
+}
+export interface Studies {
+  begin: Date;
+  end: Date | "CurrentlyStudyingHere";
+  title: string;
+  degree: string;
+  description: string;
+}
+
+export interface CVData {
   data: object;
-  setSession: (session: object) => void;
-  logOff: () => void;
+  setData: (session: object) => void;
 }
 
 const useCVData = create(
-  persist<Session>(
+  persist<CVData>(
     (set) => ({
       data: {},
       setData: (data) => {
@@ -17,13 +58,12 @@ const useCVData = create(
     }),
     {
       name: "session-storage",
-      storage: createJSONStorage(() => sessionStorage),
+      storage: createJSONStorage(() => localStorage),
     }
   )
 );
 
 export default useCVData;
-
 
 /*
 datos (los justos)
