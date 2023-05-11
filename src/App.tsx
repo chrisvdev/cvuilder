65d5e7a326dba6c5b34c6af6e938d47e4f3fe456
Christian Villegas <chrisvill.2312@gmail.com>
2023-05-10 23:10:43 -0300

    Stream 10/5

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import useCVData from '@/store/CVData'
 
 export default function App() {
+  const {data} = useCVData()
+  console.log(data)
   return (
-    <div>App</div>
+    <div>
+      probando cositas que Juan Pablo me dice 🥰
+    </div>
   )
 }
--- src/store/CVData.ts
+++ src/store/CVData.ts
@@ -1,31 +1,29 @@
 import { create } from "zustand";
 import { persist, createJSONStorage } from "zustand/middleware";
-import { CVData, GlobalState } from "@/store/cv.types";
+import { GlobalMethods, GlobalState, PersonalData } from "@/store/cv.types";
 
 const useCVData = create(
   persist<GlobalState>(
     (set) => ({
-      data: {
-        aboutMe: "About me...",
-        languages: ["english"],
-        personalData: {
-          firstName: "name",
-          email: "example@mail.dev",
-          lastName: "last name",
-          residency: "Some city",
-        },
-        studies: [
-          {
-            begin: new Date(),
-            end: new Date(),
-            degree: "Degree",
-            description: "Description",
-            title: "title",
-          },
-        ],
+      aboutMe: "About me...",
+      languages: ["english"],
+      personalData: {
+        firstName: "name",
+        email: "example@mail.dev",
+        lastName: "last name",
+        residency: "Some city",
       },
-      setData: (data: CVData) => {
-        set({ data });
+      studies: [
+        {
+          begin: new Date(),
+          end: new Date(),
+          degree: "Degree",
+          description: "Description",
+          title: "title",
+        },
+      ],
+      setPersonalData: (personalData: PersonalData) => {
+        set({ personalData });
       },
     }),
     {
@@ -36,3 +34,5 @@ const useCVData = create(
 );
 
 export default useCVData;
+
+export const getAboutMe = (state) => state.aboutMe;
