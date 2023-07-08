54fac107ca4af233b46491a88d1c38142dc95415
Christian Villegas <chrisvill.2312@gmail.com>
2023-07-08 16:02:41 -0300

    FIX: :lock: Tokens renew and moved to .env file

--- src/firebase/app.ts
+++ src/firebase/app.ts
@@ -5,14 +5,16 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDCrYEQwTd1bc5iFISFrnRnuOfXNxYYueQ",
-    authDomain: "cvuilder-44596.firebaseapp.com",
-    projectId: "cvuilder-44596",
-    storageBucket: "cvuilder-44596.appspot.com",
-    messagingSenderId: "435309653170",
-    appId: "1:435309653170:web:e98b0247a8fb0bdda62dab"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 };
+
+console.log(firebaseConfig);
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export default app
+export default app;
